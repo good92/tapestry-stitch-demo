9eb414e6f493c04d13808a876282d32d18d87104
uklance <uklance@gmail.com>
2012-11-11 21:38:07 +0000

    Change rowIndex to be <tr> index

--- src/main/java/org/lazan/t5/stitch/demo/pages/GridDecoratorDemo.java
+++ src/main/java/org/lazan/t5/stitch/demo/pages/GridDecoratorDemo.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.tapestry5.dom.Element;
-import org.lazan.t5.stitch.model.CellDecorator;
-import org.lazan.t5.stitch.model.RowDecorator;
+import org.lazan.t5.stitch.model.GridCellDecorator;
+import org.lazan.t5.stitch.model.GridRowDecorator;
 
 public class GridDecoratorDemo {
 	public static class Item {
@@ -29,8 +29,8 @@ public class GridDecoratorDemo {
 		return items;
 	}
 	
-	public RowDecorator getRowDecorator() {
-		return new RowDecorator() {
+	public GridRowDecorator getRowDecorator() {
+		return new GridRowDecorator() {
 			public void decorate(Element element, Object rowValue, int rowIndex) {
 				Item item = (Item) rowValue;
 				String script = String.format("alert('value=%s, rowIndex=%s')", item.value, rowIndex);
@@ -39,14 +39,14 @@ public class GridDecoratorDemo {
 		};
 	}
 	
-	public CellDecorator getCellDecorator() {
-		return new CellDecorator() {
+	public GridCellDecorator getCellDecorator() {
+		return new GridCellDecorator() {
 			public void decorate(Element cellElement, Object rowObject, int rowIndex, String propertyName, int colIndex) {
 				String color;
 				if (rowIndex % 2 == 0) {
-					color = colIndex % 2 == 0 ? "green" : "red";
+					color = colIndex % 2 == 0 ? "yellow" : "red";
 				} else {
-					color = colIndex % 2 == 0 ? "pink" : "blue";
+					color = colIndex % 2 == 0 ? "green" : "blue";
 				}
 				cellElement.attribute("style", "background-color:" + color);
 			}
